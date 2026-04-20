f711f10929346d7df508db5ac421010596efba00
Lasyasree999 <sree09022004@gmail.com>
2026-04-20 14:42:15 +0530

    Update app.py

--- Automatic Image Thumbnail Generator/app.py
+++ Automatic Image Thumbnail Generator/app.py
@@ -140,4 +140,6 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # host='0.0.0.0' allows external access
+    # port=5000 specifies the port
+    app.run(host='0.0.0.0', port=5000, debug=True)
